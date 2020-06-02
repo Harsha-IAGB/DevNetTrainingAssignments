c8f48a8cd0d25d62adac2c1c4a27018501d6a216
Harsha-IAGB <sriharshav99@gmail.com>
2020-06-02 16:16:09 +0530

    GETing network devices data from API

---
+++ devices_from_api_tester.py
@@ -0,0 +1,14 @@
+import unittest, json
+from network_devices_data import DevicesAPI
+from generic_parser import Parser
+
+class APITesterMethods(unittest.TestCase):
+    def test_get_data(self):
+        username = "dnacdev"
+        password = "D3v93T@wK!"
+        expected_output = json.dumps(Parser("Output/devices_from_api.json").get_parsed_data())
+        actual_output = json.dumps(DevicesAPI(username, password).get_device_data())
+        self.assertEqual(actual_output, expected_output)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ network_devices_data.py
@@ -0,0 +1,25 @@
+import requests, json
+from requests.auth import HTTPBasicAuth
+
+class DevicesAPI():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+        token = self.login()
+
+        self.device_data = self.get_device_data_from_API(token)
+
+    def get_device_data_from_API(self, token):
+        headers = {
+            'x-auth-token':token
+        }
+        response = requests.get("https://sandboxdnac2.cisco.com/dna/intent/api/v1/network-device", headers=headers)
+        return json.loads(response.content)
+
+    def login(self):
+        response = requests.post('https://sandboxdnac2.cisco.com/api/system/v1/auth/token', auth=HTTPBasicAuth(self.username, self.password))
+        return json.loads(response.content)['Token']
+
+    def get_device_data(self):
+        return self.device_data
